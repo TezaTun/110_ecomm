dc83c891de286c68f68a7484b6404264eada0da1
Teza <teoisweet@gamil.com>
2025-07-22 15:35:04 +0630

    database

--- admin/adminlogin.php
+++ admin/adminlogin.php
@@ -1,6 +1,12 @@
 <?php
 
+if(isset($_POST['login']))
+{
+$email = $_POST['email'];
+$password = $_POST['password'];
 
+echo"email is $email and password you typed is $password";
+}
 
 
 ?>
@@ -21,14 +27,15 @@
         require_once("navbarcopy.php");
         ?>
     </div>
+
     <div class="row">
         <div class="col-md-6 mx-auto py-5">
         <form action= "adminlogin.php" method="post">
             <div class="mb-3">
                 <label for="" class="form-label">Email</label>
                 <input type="email" class="form-control" name="email">
-
             </div>
+
             <div class="mb-3">
                 <label for="">Password</label>
                 <input type="password" class="form-control" name="password">
---
+++ admin/dbconnect.php
@@ -0,0 +1,17 @@
+<?php
+$hostname="localhost";
+$username= "root";
+$password= "";
+$dbname= "golden_city";
+$dbn = "mysql:host=$hostname; dbname=$dbname";
+try {
+    $conn = new PDO($dbn, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    echo"connection got";
+
+}catch(Exception $e) {  
+    echo $e->getMessage();
+}
+
+?>
